a7c78c0d395dbbf6c609f146ca0787f9666acb66
HenryMartin <phileaitxro@gmail.com>
2022-01-06 02:39:32 +0800

    update

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const puppeteer = require('puppeteer-extra')
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 const { osLocale } = require('os-locale-s-fix')
 
+require('puppeteer-extra-plugin-stealth/evasions/chrome.app')
+require('puppeteer-extra-plugin-stealth/evasions/chrome.csi')
+require('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes')
+require('puppeteer-extra-plugin-stealth/evasions/chrome.runtime')
+require('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow')
+require('puppeteer-extra-plugin-stealth/evasions/media.codecs')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.languages')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.permissions')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.plugins')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.vendor')
+require('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver')
+require('puppeteer-extra-plugin-stealth/evasions/sourceurl')
+require('puppeteer-extra-plugin-stealth/evasions/user-agent-override')
+require('puppeteer-extra-plugin-stealth/evasions/webgl.vendor')
+require('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions')
+require('puppeteer-extra-plugin-stealth/evasions/defaultArgs')
+
 const s = (ss) =>
   new Promise((res, rej) => {
     setTimeout(res, ss * 1000)
@@ -18,6 +36,20 @@ const s30 = () =>
   new Promise((res, rej) => {
     return setTimeout(res, 30 * 1000)
   })
+
+function randomNum(minNum, maxNum) {
+  switch (arguments.length) {
+    case 1:
+      return parseInt(Math.random() * minNum + 1, 10)
+      break
+    case 2:
+      return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10)
+      break
+    default:
+      return 0
+      break
+  }
+}
 
 Date.prototype.format = function (fmt) {
   var o = {
@@ -44,8 +76,10 @@ Date.prototype.format = function (fmt) {
 }
 
 const maxenergy = 0 // You can set the to 150
-
-async function play(account = 'gagxi.wam', pos, module) {
+let serverurl = 'https://api.waxsweden.org'
+//  'https://api.waxsweden.org/v1/chain/get_table_rows'
+// 'https://api.wax.alohaeos.com/v1/chain/get_table_rows'
+async function play(account, pos, module, salelist) {
   let browser = null
   const position = pos * 150
   const DefaultTimeout = 90 * 1000
@@ -54,8 +88,8 @@ async function play(account = 'gagxi.wam', pos, module) {
     const params = {
       headless: false,
       args: [
-        "--proxy-server='direct://'",
-        '--proxy-bypass-list=*',
+        // "--proxy-server='direct://'",
+        // '--proxy-bypass-list=*',
         '--ignore-certificate-errors',
         '--no-sandbox',
         '--disable-infobars',
@@ -101,6 +135,8 @@ async function play(account = 'gagxi.wam', pos, module) {
       if (consoleObj.text().indexOf('inner-') === 0) {
         const key = consoleObj.text().replace('inner-', '')
         console.log(i18n.__(key), new Date().format(' yyyy-MM-dd hh:mm:ss'))
+        // } else {
+        //   console.log(consoleObj.text())
       }
     })
 
@@ -109,6 +145,16 @@ async function play(account = 'gagxi.wam', pos, module) {
 
     await page.waitForSelector('button.login-button')
     console.log(i18n.__('Login Page Load'), new Date().format(' yyyy-MM-dd hh:mm:ss'))
+    // select server
+    const serverlist = await page.$$eval('#RPC-Endpoint option', (options) =>
+      options.map((option) => option.text)
+    )
+    const selectcount = serverlist.length
+    const selectid = randomNum(0, selectcount - 2)
+    serverurl = serverlist[selectid + 1]
+    console.log('select server : ', selectid, selectcount, serverurl)
+    await page.waitForTimeout(2000)
+    page.select('#RPC-Endpoint', '' + selectid)
 
     await page.waitForTimeout(2000)
     await page.click('button.login-button')
@@ -139,6 +185,7 @@ async function play(account = 'gagxi.wam', pos, module) {
         if (await checkMapTitle(page)) {
           await click(page, account, 'Feed', 3)
           await click(page, account, 'Hatch', 3)
+
           await energy(page, account)
         } else {
           await home(page)
@@ -152,6 +199,7 @@ async function play(account = 'gagxi.wam', pos, module) {
         await selected(page, 'crop-map')
         if (await checkMapTitle(page)) {
           await click(page, account, 'Water', 8)
+
           await energy(page, account)
         } else {
           await home(page)
@@ -165,6 +213,7 @@ async function play(account = 'gagxi.wam', pos, module) {
         await selected(page, 'cow-map')
         if (await checkMapTitle(page)) {
           await click(page, account, 'Feed', 2)
+
           await energy(page, account)
         } else {
           await home(page)
@@ -175,7 +224,15 @@ async function play(account = 'gagxi.wam', pos, module) {
       }
       if (module.includes('5')) {
         await build(page)
+
         await energy(page, account)
+      }
+      if (module.includes('6') && salelist) {
+        const list = salelist.includes('-') ? salelist.split('-') : [salelist]
+        for (const title of list) {
+          await sale(page, title)
+          await home(page)
+        }
       }
       await s10()
     }
@@ -197,52 +254,55 @@ async function checkMapTitle(page) {
 }
 
 async function checkCPU(page, title) {
-  let checklist = [
-    page.waitForSelector('.modal-stake-header'),
-    page.waitForSelector('.modal__button-group>.button-section'),
-    // page.waitForSelector('.close-modal'),
-    page.waitForFunction(() => {
-      var msg = document.querySelector('div.flash-message-content')
-      if (msg) {
-        return msg.innerText.includes('successful')
-      }
-      return false
-    }),
-    page.waitForFunction(async () => {
-      var msg = document.querySelector('div.flash-message-content')
-      if (msg) {
-        if (msg.innerText.includes('Repairing')) {
-          await new Promise((resolve, reject) => setTimeout(resolve, 3000))
-          return true
+  try {
+    let checklist = [
+      page.waitForSelector('.modal-stake-header'),
+      page.waitForSelector('.modal__button-group>.button-section'),
+      // page.waitForSelector('.close-modal'),
+      page.waitForFunction(() => {
+        var msg = document.querySelector('div.flash-message-content')
+        if (msg) {
+          return msg.innerText.includes('successful')
+        }
+        return false
+      }),
+      page.waitForFunction(async () => {
+        var msg = document.querySelector('div.flash-message-content')
+        if (msg) {
+          if (msg.innerText.includes('Repairing')) {
+            await new Promise((resolve, reject) => setTimeout(resolve, 3000))
+            return true
+          }
+        }
+        return false
+      }),
+      page.waitForFunction(() => {
+        var msg = document.querySelector('div.flash-message-content')
+        if (msg) {
+          return msg.innerText.includes('Eating')
+        }
+        return false
+      }),
+    ]
+
+    let champion = await Promise.race(checklist)
+    await s(2)
+    if (champion) {
+      if (champion.constructor.name === 'ElementHandle') {
+        const stake = await champion.evaluate((node) => node.getAttribute('class'))
+        if ('modal-stake-header' === stake) {
+          const close_modal = await page.waitForSelector('.close-modal')
+          await close_modal.click()
+        } else {
+          champion.click()
         }
       }
-      return false
-    }),
-    page.waitForFunction(() => {
-      var msg = document.querySelector('div.flash-message-content')
-      if (msg) {
-        return msg.innerText.includes('Eating')
-      }
-      return false
-    }),
-  ]
-
-  let champion = await Promise.race(checklist)
-  await s(2)
-  if (champion) {
-    if (champion.constructor.name === 'ElementHandle') {
-      const stake = await champion.evaluate((node) => node.getAttribute('class'))
-      if ('modal-stake-header' === stake) {
-        const close_modal = await page.waitForSelector('.close-modal')
-        await close_modal.click()
-      } else {
-        champion.click()
-      }
-      // }else{
-      // await s(3)
+      console.log(i18n.__(title), new Date().format(' yyyy-MM-dd hh:mm:ss'))
+      await page.click('div.game-container')
     }
-    console.log(i18n.__(title), new Date().format(' yyyy-MM-dd hh:mm:ss'))
-    await page.click('div.game-container')
+    await s(2)
+  } catch (e) {
+    console.log('🚫', e)
   }
 }
 
@@ -251,10 +311,8 @@ async function selected(page, title) {
     for (const elem of element) {
       if (elem.innerText.includes('Map')) {
         await elem.click()
-        return true
       }
     }
-    return false
   })
   await s(1)
   await page.$$eval(
@@ -263,14 +321,89 @@ async function selected(page, title) {
       for (const elem of element) {
         if (elem.style['background-image'].includes(title)) {
           await elem.click()
-          return true
         }
       }
-      return false
     },
     title
   )
   await s(2)
+}
+
+// async function wear(page, title) {
+//   const wearList = [
+//     {k: 'barley seed',v: 'crops'},
+//     {k: 'corn seed',v: 'crops'},
+//     {k: '',v: 'crops'},
+//   ]
+//   const data = {
+//     json: true,
+//     code: 'farmersworld',
+//     scope: 'farmersworld',
+//     table: 'crops',
+//     lower_bound: '.auyk.wam',
+//     upper_bound: '.auyk.wam',
+//     index_position: 2,
+//     key_type: 'i64',
+//     limit: '100',
+//     reverse: false,
+//     show_payer: false,
+//   }
+
+//   const res = await axios.post('https://wax.pink.gg/v1/chain/get_table_rows', data, {
+//     timeout: 90 * 1000,
+//   })
+//   // const res = await axios.post('https://api.waxsweden.org/v1/chain/get_table_rows', data)
+//   // const res = await axios.post('https://api.wax.alohaeos.com/v1/chain/get_table_rows', data)
+//   if (res.status === 200) {
+//     const json = res.data
+//   }
+// }
+
+async function sale(page, title) {
+  title = title.toLowerCase()
+  // console.log(title)
+  await page.$$eval('div.navbar-group--tilte', async (element) => {
+    for (const elem of element) {
+      if (elem.innerText.includes('Chest')) {
+        await elem.click()
+      }
+    }
+  })
+  await s(1)
+
+  let loop = true
+  while (loop) {
+    const isClick = await page.$$eval(
+      'div.card-container',
+      async (element, title) => {
+        for (const elem of element) {
+          const text = elem.innerText.toLowerCase()
+          if (text.includes(title)) {
+            let countText = text.split(title)[0]
+            let count = parseInt(countText)
+            if (count > 0) {
+              console.log('inner-✅售卖第' + count + '个' + title)
+              const buts = await elem.querySelectorAll('button.button-section')
+              for (const but of buts) {
+                if (but.innerText.toLowerCase().includes('exchange')) {
+                  but.click()
+                  return true
+                }
+              }
+            }
+          }
+        }
+        return false
+      },
+      title
+    )
+
+    if (isClick) {
+      await checkCPU(page, title + ' Success')
+    }
+    loop = isClick
+  }
+  await s(1)
 }
 
 async function home(page) {
@@ -321,12 +454,10 @@ async function build(page) {
     }
     return false
   })
-  // console.log(isClick, isClick2, isClick3)
 }
 
 //
-async function energy(page, account = 'gagxi.wam') {
-  // if (isMint) {
+async function energy(page, account) {
   try {
     isMint = false
     process.stdout.write(
@@ -345,11 +476,10 @@ async function energy(page, account = 'gagxi.wam') {
       limit: '100',
     }
 
-    const res = await axios.post('https://wax.pink.gg/v1/chain/get_table_rows', data, {
+    const res = await axios.post(serverurl + '/v1/chain/get_table_rows', data, {
       timeout: 90 * 1000,
     })
-    // const res = await axios.post('https://api.waxsweden.org/v1/chain/get_table_rows', data)
-    // const res = await axios.post('https://api.wax.alohaeos.com/v1/chain/get_table_rows', data)
+
     if (res.status === 200) {
       const json = res.data
       let last = json['rows'][0]['max_energy'] - json['rows'][0]['energy']
@@ -386,15 +516,7 @@ async function energy(page, account = 'gagxi.wam') {
           return false
         })
         if (isClick) {
-          // let modal = await page.$('.modal-content')
           await checkCPU(page, 'Energy Success')
-          // if (!(await checkCPU(page))) {
-          //   isMint = false
-          //   await page.waitForSelector('div.flash-message-content')
-          //   console.log(i18n.__('Energy Success'), new Date().format(" yyyy-MM-dd hh:mm:ss"))
-          // } else {
-          //   await page.click('div.game-container')
-          // }
           await s(2)
         }
         await page.click('div.game-container')
@@ -403,7 +525,6 @@ async function energy(page, account = 'gagxi.wam') {
   } catch (error) {
     console.log(error.toString())
   }
-  // }
 }
 async function click(page, account, title, count = 24) {
   process.stdout.write(
@@ -414,7 +535,7 @@ async function click(page, account, title, count = 24) {
     if (tools) {
       await page.click('img[alt="' + index + '"]')
       await page.waitForTimeout(1500)
-      var timeout = await page.$eval('div.card-container--time', (card) => card.innerText)
+      var timeout = await page.$eval('.info-time .card-container--time', (card) => card.innerText)
       if (count > 9 || timeout == '00:00:00') {
         let isClick = await page.$$eval(
           'div.plain-button',
@@ -453,8 +574,8 @@ async function click(page, account, title, count = 24) {
     const args = process.argv.splice(2)
     let account = args[0] || ''
     const position = Number(args[1] || 0)
-    const module = args[2] || '12345'
-
+    let module = args[2]
+    let salelist = args[3] || ''
     if (!account) {
       const response = await prompts({
         type: 'text',
@@ -472,12 +593,24 @@ async function click(page, account, title, count = 24) {
       })
       account = response.account
     }
+    if (!module) {
+      const response = await prompts({
+        type: 'text',
+        name: 'module',
+        message: i18n.__(
+          'Select'
+        ),
+      })
+      module = response.module
+    }
+
+    module = module || '12345'
 
     while (true) {
       try {
         if (account) {
           console.log(i18n.__('Waiting'))
-          await play(account, position, module)
+          await play(account, position, module, salelist)
         }
       } catch (error) {
         console.log(i18n.__('Offline'), new Date().format(' yyyy-MM-dd hh:mm:ss'))
